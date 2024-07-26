9048a1e88f519e3a8fc5a37c47a8e2d5d99e2da0
Aan Dono <aandono1010@gmail.com>
2024-07-27 04:42:07 +0700

    Initial commit

---
+++ js/naziados.js
@@ -0,0 +1,107 @@
+'use strict';
+
+(function () {
+
+    let lastZIndex = 0;
+
+    function getLastZIndex () {
+        return Math.max(...Array.from(document.querySelectorAll('*')).map(element => {
+          const zIndex = window.getComputedStyle(element).zIndex;
+          return zIndex === 'auto' ? -1 : parseInt(zIndex);
+        }));
+    }
+
+    function initCloseButtons () {
+        document.addEventListener('click', function(event) {
+            let closeButton = event.target.closest('.box-close');
+            if (closeButton) {
+                let box = closeButton.parentNode.parentNode.parentNode;
+                box.style.display = 'none';
+            }
+        });
+    }
+
+    function initMinimizeButtons () {
+        document.addEventListener('click', function(event) {
+            let minimizeButton = event.target.closest('.box-minimize');
+            if (minimizeButton) {
+                let box = minimizeButton.parentNode.parentNode.parentNode;
+                box.querySelectorAll('.box-body').forEach(function (box) {
+                    box.style.display = 'none';
+                });
+            }
+        });
+    }
+
+    function initMaximizeButtons () {
+        document.addEventListener('click', function(event) {
+            let maximizeButton = event.target.closest('.box-maximize');
+            if (maximizeButton) {
+                let box = maximizeButton.parentNode.parentNode.parentNode;
+                box.querySelectorAll('.box-body').forEach(function (box) {
+                    box.style.display = 'flex';
+                    box.style.direction = 'column';
+                });
+            }
+        });
+    }
+
+    function initDraggableBox() {
+        document.addEventListener('mousedown', function(event) {
+            const boxTitle = event.target.closest('.box-title');
+            if (boxTitle) {
+                let box = boxTitle.parentNode.parentNode.parentNode;
+                if (box.classList.contains('box-draggable')) {
+
+                    let nextZIndex = lastZIndex++;
+
+                    const initialX = event.clientX;
+                    const initialY = event.clientY;
+
+                    const initialDivX = box.offsetLeft;
+                    const initialDivY = box.offsetTop;
+
+                    box.style.zIndex = nextZIndex;
+
+                    const onMouseMove = function(e) {
+                        const newX = initialDivX + (e.clientX - initialX);
+                        const newY = initialDivY + (e.clientY - initialY);
+
+                        box.style.position = 'absolute';
+                        box.style.left = `${newX}px`;
+                        box.style.top = `${newY}px`;
+                    };
+
+                    const onMouseUp = function(e) {
+                        document.removeEventListener('mousemove', onMouseMove);
+                        document.removeEventListener('mouseup', onMouseUp);
+                    };
+
+                    document.addEventListener('mousemove', onMouseMove);
+                    document.addEventListener('mouseup', onMouseUp);
+                }
+            }
+        });
+    }
+
+    function initBoxButtons () {
+        initCloseButtons();
+        initMinimizeButtons();
+        initMaximizeButtons();
+    }
+
+    function initBox () {
+        initBoxButtons();
+        initDraggableBox();
+    }
+
+    function init () {
+        initBox();
+    }
+
+    function main () {
+        init();
+    }
+
+    main();
+})();
